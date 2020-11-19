4c5dbd635b4c1e68e2f36c2b78749669932401e4
Bradfisch <wobrit00@hs-esslingen.de>
2020-11-19 21:34:50 +0000

    christmastree and NStep

---
+++ NStep/src/main.rs
@@ -0,0 +1,70 @@
+fn NStep(x: i32, y: i32) -> Result<i32, &'static str> {
+    if x == y || (x - 2) == y
+    {
+        Ok(x + y - if x % 2 == 0 { 0 } else { 1 })
+    }
+    else
+    {
+        Err("No Number")    
+    }
+}
+
+fn main() {
+    NStep(0, 1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[test]
+    fn When_NStep__Given_XZeroYZero__Expect_Zero() -> Result<(), String>
+    {
+        assert_eq!(NStep(0, 0)?, 0);
+        Ok(())
+    }
+
+    #[rstest(
+        x,
+        y,
+        expected,
+        case(1, 1, 1),
+        case(2, 0, 2),
+        case(3, 1, 3),
+        case(2, 2, 4),
+        case(4, 2, 6),
+        case(3, 3, 5),
+        case(5, 3, 7),
+        case(4, 4, 8),
+        case(6, 4, 10),
+        case(5, 5, 9),
+        case(7, 5, 11),
+        case(6, 6, 12)
+    )]
+    fn When_NStep__Given_GoodXY_Expect_Number(x: i32, y: i32, expected: i32) -> Result<(), String>
+    {
+        assert_eq!(NStep(x, y)?, expected);
+        Ok(())
+    }
+
+    #[rstest(
+        x,
+        y,
+        case(0, 1),
+        case(0, 2),
+        case(0, 3),
+        case(0, 4),
+        case(0, 5),
+        case(1, 0),
+        case(3, 0),
+        case(4, 0),
+        case(5, 0),
+        case(6, 0)
+    )]
+    fn When_NStep__Given_BadXY_Expect_NoNumber(x: i32, y: i32) -> Result<(), String>
+    {
+        assert_eq!(NStep(x, y), Err("No Number"));
+        Ok(())
+    }
+}
---
+++ christmastree/src/main.rs
@@ -0,0 +1,71 @@
+use clap::{App, Arg};
+
+struct ChristmasTree{
+    height: u32,
+    has_star: bool,
+}
+
+impl ChristmasTree{
+    fn draw(&self) {
+        if self.has_star {
+            self.draw_star();
+        }
+        self.draw_tree();
+        self.draw_trunk();
+    }
+
+    fn draw_star(&self) {
+        self.output("*");
+    }
+
+    fn draw_tree(&self) {
+        for i in 0..self.height {
+            let level_width = (i * 2 + 1) as usize;
+            self.output(&std::iter::repeat("X").take(level_width).collect::<String>());
+        }
+    }
+
+    fn draw_trunk(&self) {
+        self.output("I")
+    }
+    
+    fn output(&self, a: &str) {
+        println!("{}", format!("{: ^1$}", a, self.width() as usize));
+    }
+
+    fn width(&self) -> u32 {
+        self.height * 2 - 1
+    }
+}
+
+fn main() {
+    let matches = App::new("christmastree")
+        .version("0.1.0")
+        .author("wolfi")
+        .about("prints a pretty christmas tree (merry christmas!)")
+        .arg(
+            Arg::with_name("withStar")
+                .short("w")
+                .long("--withStar")
+                .help("add a little start on-top"),
+        )
+        .arg(
+            Arg::with_name("height")
+                .value_name("HEIGHT")
+                .index(1)
+                .required(true)
+                .help("height of the tree"),
+        )
+        .get_matches();
+
+    let tree = ChristmasTree {
+        height: matches
+            .value_of("height")
+            .expect("Missing argument HEIGHT")
+            .parse::<u32>()
+            .expect("Unable to parse integer"),
+        has_star: matches.is_present("withStar"),
+    };
+
+    tree.draw();
+}
